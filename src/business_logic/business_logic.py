33eeefb431dbfe04eb8b06ff3783a24f3cecc230
goncalobraga27 <a97541@alunos.uminho.pt>
2026-06-11 23:18:50 +0100

    [Feat] Registo de pedidos nos utilizadores e de faturas nos fornecedores

--- src/business_logic/business_logic.py
+++ src/business_logic/business_logic.py
@@ -7,8 +7,12 @@ class BusinessLogic:
     def __init__(self):
         self.menu = Menu()
         self.path_to_users_data = "../data/users_data.csv"
+        self.path_to_request_data = "../data/request_data.csv"
+        self.path_to_invoices_data = "../data/invoices_data.csv"
         os.makedirs(os.path.dirname(self.path_to_users_data), exist_ok=True)
-    
+        os.makedirs(os.path.dirname(self.path_to_request_data), exist_ok=True)
+        os.makedirs(os.path.dirname(self.path_to_invoices_data), exist_ok=True)
+
     def encrypt(self, text):
         return base64.b64encode(text.encode()).decode()
     
@@ -33,19 +37,29 @@ class BusinessLogic:
                     return True
 
         return None
+    
+    def write_request_data(self, request_name, request_description, username):
+        with open(self.path_to_request_data, "a", encoding="utf-8") as file:
+            file.write(f"{request_name},{request_description},{username}\n")
+
+    def write_invoice_data(self, invoice_date, invoice_reference, invoice_category, invoice_payment_method, invoice_amount, username):
+        with open(self.path_to_invoices_data, "a", encoding="utf-8") as file:
+            file.write(f"{invoice_date},{invoice_reference},{invoice_category},{invoice_payment_method},{invoice_amount},{username}\n")
 
     def choose_execution_flow(self, option):
         if option == 1:
             username, password = self.menu.print_open_session()
             if self.authenticate_user(username, password):
-                self.menu.print_client_menu()
+                option = self.menu.print_client_menu()
+                self.choose_execution_flow_user(int(option), username)
             else:
                 self.menu.print_error_authentication()
     
         elif option == 2:
             username, password = self.menu.print_open_session()
             if self.authenticate_user(username, password):
-                self.menu.print_supplier_menu()
+                option = self.menu.print_supplier_menu()
+                self.choose_execution_flow_supplier(int(option), username)
             else:
                 self.menu.print_error_authentication()
 
@@ -57,5 +71,26 @@ class BusinessLogic:
             enc_password = self.encrypt(password)
             with open(self.path_to_users_data, "a", encoding="utf-8") as file:
                 file.write(f"{enc_username},{enc_password}\n")            
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
+    
+    def choose_execution_flow_user(self, option, username):
+        if option == 1:
+            request_name, request_description = self.menu.print_add_request_menu()
+            self.write_request_data(request_name, request_description, username)
+        elif option == 2:
+            invoice_date, invoice_reference, invoice_category, invoice_payment_method, invoice_amount = self.menu.print_view_request_info_menu()
+            self.write_invoice_data(invoice_date, invoice_reference, invoice_category, invoice_payment_method, invoice_amount, username)
+        elif option == 3:
+            return
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
+
+    def choose_execution_flow_supplier(self, option, username):
+        if option == 1:
+            invoice_date, invoice_reference, invoice_category, invoice_payment_method, invoice_amount = self.menu.print_view_request_info_menu()
+            self.write_invoice_data(invoice_date, invoice_reference, invoice_category, invoice_payment_method, invoice_amount, username)
+        elif option == 2:
+            return
         else:
             print("Opção inválida. Por favor, selecione uma opção válida.")
--- src/menu/menu.py
+++ src/menu/menu.py
@@ -15,11 +15,15 @@ class Menu:
         print("1. Criar pedido")
         print("2. Ver informações disponíveis sobre o pedido")
         print("3. Sair")
+        option = input("Opção: ")
+        return option
 
     def print_supplier_menu(self):
         print("Menu específico do fornecedor:")
         print("1. Criar fatura")
         print("2. Sair")
+        option = input("Opção: ")
+        return option
     
     def print_user_menu(self):
         print("Menu específico do utilizador da ferramenta:")
@@ -28,7 +32,9 @@ class Menu:
         print("4. Visualizar insights sobre os pedidos")
         print("5. Visualizar insights sobre as faturas")
         print("6. Sair")
-    
+        option = input("Opção: ")
+        return option
+
     def print_create_account(self):
         username = input("Insira o nome de utilizador: ")
         password = input("Insira a palavra-passe: ")
@@ -41,3 +47,18 @@ class Menu:
     
     def print_error_authentication(self):
         print("Autenticação mal-sucedida!")
+
+    def print_add_request_menu(self):
+        request_name = input("Insira o nome do pedido: ")
+        request_description = input("Insira a descrição do pedido: ")
+        print(f"Pedido '{request_name}' criado com sucesso!")
+        return request_name, request_description
+    
+    def print_view_request_info_menu(self):
+        invoice_date = input("Insira a data da fatura (DD/MM/AAAA): ")
+        invoice_reference = input("Insira a referência da fatura: ")
+        invoice_category = input("Insira a categoria da fatura: ")
+        invoice_payment_method = input("Insira o método de pagamento da fatura: ")
+        invoice_amount = input("Insira o valor da fatura: ")
+        print(f"Fatura '{invoice_reference}' criada com sucesso!")
+        return invoice_date, invoice_reference, invoice_category, invoice_payment_method, invoice_amount
